fe8b6dfb6e54ac97929720f2e7301a21e4cb1f85
nickthathasnotbeentakenyet <lan18029@byui.edu>
2019-05-29 22:50:10 +0300

    responsive menu & images

---
+++ assignments/lesson-6/js/currentdate.js
@@ -0,0 +1,4 @@
+var d = new Date();
+var day = new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday");
+var month = new Array("January","February","March","April","May","June","July","August","September","October","November","December");
+document.write(day[d.getDay()] + " " + d.getDate() + " of " + month[d.getMonth()] + ", " + d.getFullYear());
