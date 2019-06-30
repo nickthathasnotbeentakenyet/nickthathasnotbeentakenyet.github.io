86c6ce61ab13a62352023cd038cb6f64fc6225da
nickthathasnotbeentakenyet <lan18029@byui.edu>
2019-06-30 20:08:28 +0300

    forecast changes

--- assignments/lesson-10/js/prestonevents.js
+++ assignments/lesson-10/js/prestonevents.js
@@ -11,16 +11,16 @@ request.onload = function () {
     const output = document.getElementById('prestonevents');
     towns.forEach(town => {
         if (town.name == 'Preston') {
-            const head = document.createElement('h2');
+            // const head = document.createElement('h2');
             const para = document.createElement('p');
             const para2 = document.createElement('p');
             const para3 = document.createElement('p');
 
-            head.textContent = "Preston Town Events"
+            // head.textContent = "Preston Town Events"
             para.textContent = town.events[0];
             para2.textContent = town.events[1];
             para3.textContent = town.events[2];
-            output.appendChild(head);
+            // output.appendChild(head);
             output.appendChild(para);
             output.appendChild(para2);
             output.appendChild(para3);
