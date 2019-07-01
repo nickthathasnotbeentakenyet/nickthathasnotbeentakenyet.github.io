961b4c875b2696537ae74eb2b8ed0c5715da4740
nickthathasnotbeentakenyet <lan18029@byui.edu>
2019-07-02 01:00:08 +0300

    preston events js changes

--- assignments/lesson-10/js/prestonevents.js
+++ assignments/lesson-10/js/prestonevents.js
@@ -5,28 +5,15 @@ request.responseType = 'json';
 request.send();
 
 request.onload = function () {
-    const towndata = request.response;
-    const towns = towndata['towns'];
-
-    const output = document.getElementById('prestonevents');
+    let myList = document.getElementById('eventsPreston');
+    let towns = request.response['towns'];
     towns.forEach(town => {
         if (town.name == 'Preston') {
-            // const head = document.createElement('h2');
-            const para = document.createElement('p');
-            const para2 = document.createElement('p');
-            const para3 = document.createElement('p');
-
-            // head.textContent = "Preston Town Events"
-            para.textContent = town.events[0];
-            para2.textContent = town.events[1];
-            para3.textContent = town.events[2];
-            // output.appendChild(head);
-            output.appendChild(para);
-            output.appendChild(para2);
-            output.appendChild(para3);
-
-
+            for (let i = 0; i < town.events.length; i++) {
+                let listItem = document.createElement('li');
+                listItem.textContent = town.events[i];
+                myList.appendChild(listItem);
+            }
         }
-
     });
-};
+}
