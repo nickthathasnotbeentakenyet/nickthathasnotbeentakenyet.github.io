9988ad551e3b4646fcc6c5214a0c38287ec09efc
nickthathasnotbeentakenyet <lan18029@byui.edu>
2019-07-05 20:25:24 +0300

    weather site project updates

---
+++ assignments/weathersite/js/fishhavenevents.js
@@ -0,0 +1,19 @@
+const requestURL = "https://nickthathasnotbeentakenyet.github.io/assignments/weathersite/js/home.json";
+const request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function () {
+    let myList = document.getElementById('eventsfishhaven');
+    let towns = request.response['towns'];
+    towns.forEach(town => {
+        if (town.name == 'Fish Haven') {
+            for (let i = 0; i < town.events.length; i++) {
+                let listItem = document.createElement('li');
+                listItem.textContent = town.events[i];
+                myList.appendChild(listItem);
+            }
+        }
+    });
+}
---
+++ assignments/weathersite/js/sodaspringsforecast.js
@@ -0,0 +1,33 @@
+// --------------------------------------- 5 forecast FORECAST --------------------------------
+
+const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&APPID=6e9bcf94156c7c5e7c11e06a172b2d8a";
+const forecast = new XMLHttpRequest();
+forecast.open('GET', forecastURL, true);
+forecast.send();
+
+forecast.onload = function () {
+    const forecastData = JSON.parse(forecast.responseText);
+    console.log(forecastData);
+
+    let weekday = new Array(7);
+    weekday[0] = "Monday";
+    weekday[1] = "Tuesday";
+    weekday[2] = "Wednesday";
+    weekday[3] = "Thursday";
+    weekday[4] = "Friday";
+    weekday[5] = "Saturday";
+    weekday[6] = "Sunday";
+    let count = 1;
+    for (let i = 0; i < forecastData.list.length; i++) {
+        if (forecastData.list[i].dt_txt.includes("18:00:00")) {
+            document.getElementById('forecasttemp'+count).innerHTML=forecastData.list[i].main.temp + ' &#8457;';
+            document.getElementById('forecasticon' + count).setAttribute('src', "https://openweathermap.org/img/w/" + forecastData.list[i].weather[0].icon + ".png");
+            document.getElementById('forecasticon' + count).setAttribute('alt', forecastData.list[i].weather[0].description);
+            document.getElementById('dayforecast' + count).innerHTML = weekday[(new Date().getDay() + count - 1) % 7];
+            count++;
+        }
+        if (count == 6) {
+            break;
+        }
+    }
+}
